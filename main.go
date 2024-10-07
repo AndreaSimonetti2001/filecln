12683a1c0ced0ec8eb28c77c3f45b9206c1e9735
Andrea Simonetti <andrea.simonetti4@studio.unibo.it>
2024-10-08 00:15:52 +0200

    Full conversion in Go, changed license to MPL 2.0

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	addr := os.Args[1]
+	fmt.Printf("SERVER ADDRESS IS %v\n", addr)
+
+	path := os.Args[2]
+	fmt.Printf("PATH IS %v\n", path)
+
+	stream, _ := net.Dial("tcp", fmt.Sprintf("%v:1919", addr))
+	fmt.Printf("CONNECTED TO %v\n", stream.RemoteAddr())
+
+	file, _ := os.Open(path)
+	filename := filepath.Base(path)
+	fmt.Printf("FILE %v OPENED\n", filename)
+
+	stream.Write([]byte(fmt.Sprintf("%v\n", filename)))
+	fmt.Printf("FILENAME SENT\n")
+
+	br := bufio.NewReader(stream)
+	msg, _ := br.ReadString(byte('\n'))
+	msg = strings.TrimSpace(msg)
+	fmt.Printf("%v RECEIVED FROM THE SERVER\n", msg)
+
+	bytes, _ := io.Copy(stream, file)
+
+	fmt.Printf("SENT %v BYTES\n", bytes)
+	stream.Close()
+
+	fmt.Printf("DONE\n")
+
+}
